473e63c8b885bafe8ac283863f70ffc6f6deeda1
chendeq <chendeq@landicorp.com>
2017-10-26 17:29:00 +0800

    add by cdq 171026 17:28

--- app/src/main/java/com/tiandu/recruit/stud/ui/login/LoginActivity.java
+++ app/src/main/java/com/tiandu/recruit/stud/ui/login/LoginActivity.java
@@ -1,20 +1,33 @@
 package com.tiandu.recruit.stud.ui.login;
 
 
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.tiandu.recruit.stud.MainActivity;
 import com.tiandu.recruit.stud.R;
 import com.tiandu.recruit.stud.base.BaseActivity;
 import com.tiandu.recruit.stud.base.utils.AStringUtil;
+import com.tiandu.recruit.stud.base.utils.DialogFactory;
 import com.tiandu.recruit.stud.base.utils.SpUtil;
 import com.tiandu.recruit.stud.ui.RegisterActivity;
 import com.tiandu.recruit.stud.view.ClearEditText;
+import com.umeng.socialize.PlatformConfig;
+import com.umeng.socialize.UMAuthListener;
+import com.umeng.socialize.UMShareAPI;
+import com.umeng.socialize.UMShareConfig;
+import com.umeng.socialize.bean.SHARE_MEDIA;
+import com.umeng.socialize.utils.SocializeUtils;
 
 
+import java.util.Map;
 
 import butterknife.BindString;
 import butterknife.BindView;
@@ -38,6 +51,10 @@ public class LoginActivity extends BaseActivity{
     TextView tvForget;
     /*@BindView(R.id.cbProtocol)CheckBox cbProtocol;*/
     //@BindView(R.id.tvFirstLogin) TextView tvFirstLogin;
+    @BindView(R.id.login_qq_fast_login)
+    ImageView iv_qq_fast_login;
+    @BindView(R.id.login_we_chat_fast_login)
+    ImageView iv_wechat_fast_login;
     @BindView(R.id.etAccount) ClearEditText etAccount;
     @BindView(R.id.etPassword) ClearEditText etPassword;
     @BindString(R.string.login_loading) String loading;
@@ -45,10 +62,15 @@ public class LoginActivity extends BaseActivity{
     public static boolean isFirst;
     private String registrationID;
     private AlertDialog.Builder builder;
+    private Dialog fastDialog = null;
     @Override
     protected void initView() {
+        fastDialog = DialogFactory.getLoadingDialog(this,"跳转中",false,null);
         cannelDialog();
 //        EventBus.getDefault().register(this);
+        UMShareConfig config = new UMShareConfig();
+        config.isNeedAuthOnGetUserInfo(true);
+        UMShareAPI.get(this).setShareConfig(config);
         etAccount.setText(SpUtil.getAccount());
         isFirstLogined();
     }
@@ -109,6 +131,12 @@ public class LoginActivity extends BaseActivity{
         readyGoThenKill(MainActivity.class);
 //        presenter.userLogin(uName,uPasswd,registrationID);
     }
+    @OnClick(R.id.login_qq_fast_login) void qqFastLoginClick(){
+        UMShareAPI.get(this).doOauthVerify(this, SHARE_MEDIA.QQ, authListener);
+    }
+    @OnClick(R.id.login_we_chat_fast_login) void wechatFastClick(){
+        UMShareAPI.get(this).doOauthVerify(this, SHARE_MEDIA.WEIXIN, authListener);
+    }
     @Override
     protected void initPresenter() {
 //        presenter.setVM(this,mModel);
@@ -119,4 +147,54 @@ public class LoginActivity extends BaseActivity{
         return true;
     }
 
+    UMAuthListener authListener = new UMAuthListener() {
+        /**
+         * @desc 授权开始的回调
+         * @param platform 平台名称
+         */
+        @Override
+        public void onStart(SHARE_MEDIA platform) {
+            SocializeUtils.safeShowDialog(fastDialog);
+        }
+
+        /**
+         * @desc 授权成功的回调
+         * @param platform 平台名称
+         * @param action 行为序号，开发者用不上
+         * @param data 用户资料返回
+         */
+        @Override
+        public void onComplete(SHARE_MEDIA platform, int action, Map<String, String> data) {
+            SocializeUtils.safeCloseDialog(fastDialog);
+            showToast("成功");
+        }
+
+        /**
+         * @desc 授权失败的回调
+         * @param platform 平台名称
+         * @param action 行为序号，开发者用不上
+         * @param t 错误原因
+         */
+        @Override
+        public void onError(SHARE_MEDIA platform, int action, Throwable t) {
+            SocializeUtils.safeCloseDialog(fastDialog);
+            showToast("失败：" + t.getMessage());
+        }
+
+        /**
+         * @desc 授权取消的回调
+         * @param platform 平台名称
+         * @param action 行为序号，开发者用不上
+         */
+        @Override
+        public void onCancel(SHARE_MEDIA platform, int action) {
+            SocializeUtils.safeCloseDialog(fastDialog);
+            showToast("取消");
+        }
+    };
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        UMShareAPI.get(this).onActivityResult(requestCode, resultCode, data);
+    }
 }
